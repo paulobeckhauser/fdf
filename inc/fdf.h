f0efe42cccf514e14916cf8b3705694acf42146c
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s12d58.42wolfsburg.de>
2024-02-03 14:10:23 +0100

    remove unecessary files

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 18:10:31 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/02/03 13:10:43 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/02/03 14:09:18 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,7 +139,7 @@ void				centering(t_node *head);
 // max_min
 t_min_max			calculate_min_max(t_node *head);
 
-//check
-int check_format(char *str);
+// check
+int					check_format(char *str);
 
 #endif
